921c1cb61ab77a2ab2bbf9d3b2a53f07df5d2549
Lenovo-User <Lenovo-User@DESKTOP-66DCH94>
2024-01-17 22:32:10 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.White
+        Button1.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(316, 448)
+        Button1.Margin = New Padding(5, 4, 5, 4)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(341, 76)
+        Button1.TabIndex = 0
+        Button1.Text = "(CALCULATE YOUR AGE HERE)"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(316, 30)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(312, 32)
+        TextBox1.TabIndex = 1
+        TextBox1.Text = "  HAPPY BIRTHDAY TO YOU!"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(12F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.GhostWhite
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(1370, 600)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Font = New Font("Showcard Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Margin = New Padding(5, 4, 5, 4)
+        Name = "Form1"
+        Text = "BirthdayCalculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
